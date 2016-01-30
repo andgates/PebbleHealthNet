2561f9ccfb23f58f4048e7242612dc2835472ce5
Michael Gates <mjgates@ucsc.edu>
2016-01-30 04:14:35 -0800

    Global variables make it work

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -4,6 +4,17 @@ Window *window;
 
 TextLayer *text_layer;
 
+struct sleepMood {
+	int mood;
+	int hours;
+};
+
+
+
+struct sleepMood test;
+
+struct sleepMood test2;
+
 void init() {
 	// Create the Window
 	window = window_create();
@@ -29,14 +40,26 @@ time_t start = time_start_of_today();
 HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, 
   start, end);
 
+
+test.hours=2;
+test2.hours=2;
+
+
 if(mask == HealthServiceAccessibilityMaskAvailable) {
   // Data is available!
   APP_LOG(APP_LOG_LEVEL_INFO, "Steps today: %d", 
           (int)health_service_sum_today(metric));
+test.hours = (int)health_service_sum_today(metric);
 } else {
   // No data recorded yet today
   APP_LOG(APP_LOG_LEVEL_ERROR, "Data unavailable!");
 }
+
+persist_write_data(0, &test, sizeof(test));
+
+persist_read_data(0, &test2, sizeof(test2));
+
+APP_LOG(APP_LOG_LEVEL_INFO, "TEST: %d", test2.hours);
 }
 
 void deinit() {
